b55a117f9fca5882f6ad5774815e7e03ac6e652f
priyansh9879 <priyanshmagotra444@outlook.com>
2021-03-07 23:36:22 +0530

    Deleted files

--- ansible/fetch-hosname.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-HOST=$(sudo docker inspect -f "{{ .Config.Hostname }}" $(docker ps -q))
-
-echo "Displaying the Hostname and ip-address of all the Runing Containers:"
-echo -------------------------
-echo $HOST | tr ' ' '\n'
-echo -------------------------
--- ansible/fetch-ip.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-IP=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" $(docker ps -q))
-
-echo "Displaying the ip-address of all the Runing Containers:"
-echo -------------------------
-echo $IP | tr ' ' '\n'
-echo -------------------------
